edbb087d3fe4e191c5c361d624ec96bde04bc092
deadman360 <vmartinho.kfg@gmail.com>
2024-12-12 20:28:13 +0000

    funds

---
+++ FORM_1/arrays.js
@@ -0,0 +1,8 @@
+let nums = [5,5,5,5,7]
+let soma = 0
+nums.map
+console.log(soma)
+//map precisa retornar um valor para ser adicionado em um novo array
+//for each nao altera o array original mas nao cria nenhum array novo tambem
+//filter pede uma função que retorna um booleano e com base nesse retorno ele filtra os valores que voltam true
+//reduce recebe uma função com dois parametros um acumulador e o elemento iterado, alem da função ele recebe o valor inicial do acumulador
